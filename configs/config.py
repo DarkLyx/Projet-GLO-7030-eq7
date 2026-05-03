e237c859dafed68faa207246e5b57c719a877e9b
Lucas <lucas.gumuchian16@gmail.com>
2026-05-02 23:06:08 -0400

    [FIX] comment

--- configs/config.py
+++ configs/config.py
@@ -1,4 +1,4 @@
-MODE = "train" # "download", "preprocess", "train", "eval", "grid_search", "all"
+MODE = "train" # "download", "preprocess", "train", "eval", "grid_search", "all", "compare"
 
 APPROACH = "baseline" # "baseline", "transformers", "hybrids", "kan", "ssm"
 # Modèles disponibles selon l'approche :
